f3833d06c94becef548c0f85376e7cbd0ec60cd5
Parag Panigrahi <65505331+Picnic1209@users.noreply.github.com>
2021-07-16 17:10:14 +0530

    removed extra functions for date and time

--- popup.js
+++ popup.js
@@ -1,26 +1,4 @@
-//function to convert unixTime to human readable
-function unixToHumanReadableTime(unixTime) {
-    const milliseconds = unixTime * 1000 // 1575909015000
-    const dateObject = new Date(milliseconds)
-    const humanDateFormat = dateObject.toLocaleString() //2019-12-9 10:30:15
-    return humanDateFormat;
-}
-
-//converts millisecond to HH:MM:SS
-function convertHMS(value) {
-    const sec = parseInt(value, 10); // convert value to number if it's string
-    let hours = Math.floor(sec / 3600); // get hours
-    let minutes = Math.floor((sec - (hours * 3600)) / 60); // get minutes
-    let seconds = sec - (hours * 3600) - (minutes * 60); //  get seconds
-    // add 0 if value < 10; Example: 2 => 02
-    if (hours < 10) { hours = "0" + hours; }
-    if (minutes < 10) { minutes = "0" + minutes; }
-    if (seconds < 10) { seconds = "0" + seconds; }
-    return hours + ':' + minutes + ':' + seconds; // Return is HH : MM : SS
-}
-
 document.addEventListener('DOMContentLoaded', async () => {
-
 
     //Personal details
 
